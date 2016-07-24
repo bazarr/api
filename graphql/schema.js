1c6e6c885cf75e05aea02ac38fb100a894a78334
tasti <tasti@zakarie.com>
2016-07-23 23:28:52 -0700

    Drop graphql

--- graphql/schema.js
+++
@@ -1,9 +0,0 @@
-import {GraphQLSchema} from 'graphql/type';
-
-import mutations from './mutations';
-import queries from './queries';
-
-export default new GraphQLSchema({
-  mutation: mutations,
-  query: queries,
-});
--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 require('babel-polyfill');
 require('babel-register');
 
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-
-const schema = require('./graphql/schema').default;
-
-const app = express();
-const PORT = process.env.PORT || 8000;
-
-app.use('/', graphqlHTTP({
-  schema,
-  graphiql: true,
-}));
-
-app.listen(PORT, () => {
-  console.log('Node app is running on port', PORT);
-});
+require('./server');
